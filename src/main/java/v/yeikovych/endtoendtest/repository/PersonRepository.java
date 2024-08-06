1d9ca61d59dd42b6a749575da8ee6af1e15417fa
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-08-06 14:11:09 +0200

    Added id gen, replaced logback with slf4 added sync on put.

--- src/main/java/v/yeikovych/endtoendtest/repository/PersonRepository.java
+++ src/main/java/v/yeikovych/endtoendtest/repository/PersonRepository.java
@@ -1,5 +1,6 @@
 package v.yeikovych.endtoendtest.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -10,22 +11,28 @@ import java.util.Map;
 import java.util.Optional;
 
 @Repository
+@Slf4j
 public class PersonRepository {
 
-    private static final Logger logger = LoggerFactory.getLogger(PersonRepository.class);
-    private static final Map<Integer, Person> personDb = new HashMap<>();
+    private static final Map<Integer, Person> PERSON_DB = new HashMap<>();
+    private int nextId = 0;
 
     public Optional<Person> getById(int id) {
-        return Optional.ofNullable(personDb.get(id));
+        return Optional.ofNullable(PERSON_DB.get(id));
     }
 
-    public void add(Person person) throws IllegalArgumentException {
-        if (personDb.containsKey(person.id())) {
-            logger.info("Person [" + person + "] already exists, cannot add.");
-            throw new IllegalArgumentException("Person [" + person + "] already exists.");
-        }
+    public Person add(String name, String email) throws IllegalArgumentException {
+        Person person = new Person(nextId++, name, email);
 
-        personDb.put(person.id(), person);
-        logger.info("Person [" + person + "] successfully added to db.");
+        synchronized (PERSON_DB) {
+            if (PERSON_DB.containsKey(person.id())) {
+                log.info("Person [{}] already exists, cannot add.", person);
+                throw new IllegalArgumentException("Person [" + person + "] already exists.");
+            }
+
+            PERSON_DB.put(person.id(), person);
+            log.info("Person [{}] successfully added to db.", person);
+            return person;
+        }
     }
 }
