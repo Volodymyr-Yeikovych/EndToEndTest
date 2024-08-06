faa8a90dec9ac3ca6224620d94a457656f2dbcbb
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-08-06 14:10:48 +0200

    Added annotation, moved id gen to db.

--- src/main/java/v/yeikovych/endtoendtest/service/PersonService.java
+++ src/main/java/v/yeikovych/endtoendtest/service/PersonService.java
@@ -1,30 +1,25 @@
 package v.yeikovych.endtoendtest.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import v.yeikovych.endtoendtest.dto.PersonDto;
 import v.yeikovych.endtoendtest.model.Person;
 import v.yeikovych.endtoendtest.repository.PersonRepository;
 
-import javax.management.InstanceAlreadyExistsException;
 import java.util.NoSuchElementException;
 
 @Service
+@RequiredArgsConstructor
 public class PersonService {
 
     private final PersonRepository repository;
-    private int nextId = 0;
-
-    @Autowired
-    public PersonService(PersonRepository repository) {
-        this.repository = repository;
-    }
 
     public Person getById(int id) throws NoSuchElementException {
         return repository.getById(id).orElseThrow();
     }
 
     public void add(PersonDto dto) throws IllegalArgumentException {
-        repository.add(new Person(nextId++, dto.getName(), dto.getEmail()));
+        repository.add(dto.name(), dto.email());
     }
 }
