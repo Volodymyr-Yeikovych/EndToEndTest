75e5d9af2f1d60045e932bccd41b29f0a65f08b7
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-08-06 13:51:57 +0200

    Refactored to record.

--- src/main/java/v/yeikovych/endtoendtest/dto/PersonDto.java
+++ src/main/java/v/yeikovych/endtoendtest/dto/PersonDto.java
@@ -2,43 +2,5 @@ package v.yeikovych.endtoendtest.dto;
 
 import java.util.Objects;
 
-public class PersonDto {
-    private String name;
-    private String email;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PersonDto personDto)) return false;
-        return Objects.equals(name, personDto.name) && Objects.equals(email, personDto.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, email);
-    }
-
-    @Override
-    public String toString() {
-        return "PersonDto{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+public record PersonDto(String name, String email) {
 }
