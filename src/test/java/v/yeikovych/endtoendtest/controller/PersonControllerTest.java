7d79ee316661a02f3a8981a5aca1925a8babf99f
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-08-12 10:24:43 +0200

    Added test for put person endpoint.

--- src/test/java/v/yeikovych/endtoendtest/controller/PersonControllerTest.java
+++ src/test/java/v/yeikovych/endtoendtest/controller/PersonControllerTest.java
@@ -2,10 +2,14 @@ package v.yeikovych.endtoendtest.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import v.yeikovych.endtoendtest.dto.PersonDto;
 import v.yeikovych.endtoendtest.model.Person;
 import v.yeikovych.endtoendtest.repository.PersonRepository;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +31,15 @@ class PersonControllerTest {
         var actual = personController.getPersonById(saved.getId());
         assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.OK);
         // assert body, but for same reason it's void
+    }
+
+    @Test
+    public void addShouldAddPersonToRepository(){
+        var name = "name";
+        var email = "email";
+        var person = new PersonDto(name, email);
+
+        var actual = personController.addPerson(person);
+        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
 }
